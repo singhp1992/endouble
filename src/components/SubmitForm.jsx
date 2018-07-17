8b872f6a94c3bc79c2c0947b5b4217c92a47f506
Preeti <singhp1992@gmail.com>
2018-07-17 12:22:39 +0200

    adding date picker, gender, information

--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -8,7 +8,25 @@ class Submit extends PureComponent {
 
         return (
             <div class="form">
+           
 
+            <h1> APPLY FOR THE POSITION OF PURCHASING ASSISTANT </h1>
+
+            <h2> Personal Details * </h2>
+
+            <nav>
+            <input type="text" placeholder="First Name"></input>
+            <input type="text" placeholder="Last Name"></input>
+            <br/>
+            <input type="checkbox" name="sex" value="male"></input>Male
+            <input type="checkbox" name="sex" value="female"></input>Female
+            <input type="checkbox" name="sex" value="female"></input>Prefer not to say
+                   
+            <br/>
+            Birthday:
+            <input type="date" name="bday"></input>
+                    <input type="submit"></input>
+            </nav>
             </div>
         )
     }
