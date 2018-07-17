bb89a781aa35c43729687a8de4cc9b91c8af45f5
Preeti <singhp1992@gmail.com>
2018-07-17 11:59:09 +0200

    setting up submit form

---
+++ src/components/SubmitForm.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import './submit.css'
+
+class Submit extends PureComponent {
+
+    render() {
+
+        return (
+            <div class="form">
+
+            </div>
+        )
+    }
+}
+
+export default Submit
